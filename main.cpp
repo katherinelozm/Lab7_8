308e3b5741afbfe7b691afa8c6e0c108f3ddad3c
Katherine Lozano <katherinelozm@gmail.com>
2015-03-20 14:24:23 -0600

    Main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include "person.h"
+#include "admin.h"
+#include "investigator.h"
+#include "forence.h"
+#include "evidence.h"
+#include "thecase.h"
+#include "homicide.h"
+
 
 int main(){
-	
+	vector<
+	bool logged;
+	int logged_pos;
+	int ans;
+	do{
+		cout << "Menu" << endl;
+		cout << "1. Add Person" << endl;
+		cout << "2. Delete Person" << endl;
+		cout << "3. Login" << endl;
+		cout << "0. Exit" << endl;
+		cin >> ans;
+		if (ans==1){
+			
+		} else if (ans==2){
+			
+		} else if (ans==3){
+			//
+		}	
+	} while (ans!=0);
+	return 0;
 }
